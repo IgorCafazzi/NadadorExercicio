1ad8ca1704373e4f95686fd20d181e2097c9cae8
IgorCafazzi <igorcafazzi@gmail.com>
2024-04-21 20:17:07 -0300

    exercicio feito

---
+++ NadadorExercicio/Nadador/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Nadador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdistancia = new System.Windows.Forms.TextBox();
+            this.txtpremiacao = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "distancia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "premio";
+            // 
+            // txtdistancia
+            // 
+            this.txtdistancia.Location = new System.Drawing.Point(155, 54);
+            this.txtdistancia.Name = "txtdistancia";
+            this.txtdistancia.Size = new System.Drawing.Size(100, 20);
+            this.txtdistancia.TabIndex = 2;
+            // 
+            // txtpremiacao
+            // 
+            this.txtpremiacao.Location = new System.Drawing.Point(155, 118);
+            this.txtpremiacao.Name = "txtpremiacao";
+            this.txtpremiacao.ReadOnly = true;
+            this.txtpremiacao.Size = new System.Drawing.Size(100, 20);
+            this.txtpremiacao.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(368, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(180, 235);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "limpar";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpremiacao);
+            this.Controls.Add(this.txtdistancia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdistancia;
+        private System.Windows.Forms.TextBox txtpremiacao;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ NadadorExercicio/Nadador/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nadador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double distancia;
+            distancia = Convert.ToDouble(txtdistancia.Text);
+            if (distancia < 800)
+            {
+                txtpremiacao.Text = "Seu  prêmio é de R$5.000";
+            }
+            else if (distancia > 800 && distancia < 1500)
+            {
+                txtpremiacao.Text = "Seu  prêmio é de R$10.000";
+            }
+            else
+            {
+                txtpremiacao.Text = "Seu prêmio é de R$15.000";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            txtdistancia.Clear();
+            txtpremiacao.Clear();
+        }
+    }
+}
+        
+    
+
